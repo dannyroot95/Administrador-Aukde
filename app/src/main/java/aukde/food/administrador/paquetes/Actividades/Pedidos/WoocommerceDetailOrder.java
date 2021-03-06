6759e878eb52dfb52dc5eee571705b416d902176
dannyroot <megamanxxx678@gmail.com>
2021-03-06 03:19:35 -0500

    version 6.2

--- app/src/main/java/aukde/food/administrador/paquetes/Actividades/Pedidos/WoocommerceDetailOrder.java
+++ app/src/main/java/aukde/food/administrador/paquetes/Actividades/Pedidos/WoocommerceDetailOrder.java
@@ -6,12 +6,24 @@ import androidx.core.app.NavUtils;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import aukde.food.administrador.R;
 import aukde.food.administrador.paquetes.Inclusiones.MiToolbar;
@@ -19,7 +31,7 @@ import aukde.food.administrador.paquetes.Modelos.PedidoLlamada;
 import aukde.food.administrador.paquetes.ModelsWoocommerce.Woocommerce;
 import es.dmoral.toasty.Toasty;
 
-public class WoocommerceDetailOrder extends AppCompatActivity {
+public class WoocommerceDetailOrder extends AppCompatActivity implements OnMapReadyCallback {
 
 
     TextView name , lastName , phone , email , address , status, sku ,
@@ -27,6 +39,12 @@ public class WoocommerceDetailOrder extends AppCompatActivity {
             payMethod;
     String id;
     ImageView methodPayIMG;
+    private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
+    private MapView mapView;
+    private Geocoder geocoder;
+    private GoogleMap mMap;
+    double latitude ;
+    double longitude ;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -49,6 +67,16 @@ public class WoocommerceDetailOrder extends AppCompatActivity {
         total = findViewById(R.id.orderTotalClient);
         payMethod = findViewById(R.id.orderPayMethod);
         methodPayIMG = findViewById(R.id.imgPayMethod);
+        geocoder = new Geocoder(this);
+
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        }
+
+        mapView = findViewById(R.id.map_view);
+        mapView.onCreate(mapViewBundle);
+        mapView.getMapAsync(this);
 
         Intent intent = this.getIntent();
         Bundle bundle = intent.getExtras();
@@ -65,19 +93,19 @@ public class WoocommerceDetailOrder extends AppCompatActivity {
         data.add(woo.getBilling().getEmail());
         data.add(woo.getBilling().getAddress_1());
 
-        /*for (int i = 0 ; i<woo.getMeta_data().size() ; i++){
+        for (int i = 0 ; i<woo.getMeta_data().size() ; i++){
             if (woo.getMeta_data().get(i).getKey().equals("billing_lat")){
                 data.add(woo.getMeta_data().get(i).getValue());
-                latitude.setText(data.get(4));
+                 latitude = Double.parseDouble((data.get(7)));
             }
         }
 
         for (int j = 0 ; j<woo.getMeta_data().size() ; j++){
             if (woo.getMeta_data().get(j).getKey().equals("billing_long")){
                 data.add(woo.getMeta_data().get(j).getValue());
-                longitude.setText(data.get(5));
+                longitude = Double.parseDouble((data.get(8)));
             }
-        }*/
+        }
 
 
 
@@ -116,6 +144,19 @@ public class WoocommerceDetailOrder extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAP_VIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mapView.onSaveInstanceState(mapViewBundle);
+    }
+
     private void verifyState(){
 
         if (status.getText().toString().equals("cancelled")){
@@ -152,6 +193,70 @@ public class WoocommerceDetailOrder extends AppCompatActivity {
         else{
             methodPayIMG.setImageResource(R.drawable.paycard);
         }
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        try {
+
+            List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
+
+            if (addresses.size() > 0) {
+                Address address = addresses.get(0);
+                LatLng aukde = new LatLng(address.getLatitude(), address.getLongitude());
+                MarkerOptions markerOptions = new MarkerOptions()
+                        .position(aukde)
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.aukdemarker))
+                        .title("Cliente");
+                mMap.addMarker(markerOptions).showInfoWindow();
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(aukde, 16));
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        mapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 
 
